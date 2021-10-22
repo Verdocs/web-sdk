f7b28cf44d3dbc78a209dbbf91e27254ad3080ea
leadsoft <clients@leadsoft.ro>
2021-10-22 15:48:55 +0300

    Added Search Recent component

--- verdocs-web-sdk/src/components/elements/search-recent/search-recent.stories.tsx
+++ verdocs-web-sdk/src/components/elements/search-recent/search-recent.stories.tsx
@@ -2,10 +2,16 @@ import {html} from 'lit-html';
 import {Meta} from '@storybook/web-components';
 import {action} from '@storybook/addon-actions';
 
+const dummyData = [
+  { params: {q: 'AGNT'} }, { params: {q: 'W9'}, }, { params: {q: 'leasing agreement', type: 'template'}, }, { params: { q: 'verdocs template' } }
+]
+
 export default {
   title: 'Elements/Search/Recent',
   component: 'search-recent',
-  args: {},
+  args: {
+    options: dummyData
+  },
   argTypes: {},
 } as Meta;
 
