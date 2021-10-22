58e1458d7d70ecd6a0370ddafb191fa86b7ba287
leadsoft <clients@leadsoft.ro>
2021-10-22 15:44:34 +0300

    Added Search Quick Functions component

--- verdocs-web-sdk/src/components/elements/search-quick-functions/search-quick-functions.tsx
+++ verdocs-web-sdk/src/components/elements/search-quick-functions/search-quick-functions.tsx
@@ -1,4 +1,6 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop } from '@stencil/core';
+import TemplateIcon from './template-icon.svg';
+import DocumentIcon from './document-icon.svg';
 
 @Component({
   tag: 'search-quick-functions',
@@ -6,11 +8,24 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class SearchQuickFunctions {
+  @Prop() options: any
+
+  createTemplate() {
+    console.log('create template') //probably gonna be an API call
+  }
+
+  createDocument() {
+    console.log('create document') //probably gonna be an API call
+  }
 
   render() {
     return (
       <Host>
-        <slot></slot>
+        <div class="container">
+          <p class="title">{this.options.title}</p>
+          <button onClick={() => this.createTemplate()} innerHTML={TemplateIcon} class="buttons">Template</button>
+          <button onClick={() => this.createDocument()} innerHTML={DocumentIcon} class="buttons">Document</button>
+        </div>
       </Host>
     );
   }
