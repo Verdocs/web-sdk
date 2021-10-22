f53a89f56b8f73e178e79e3281b311b0aa41c9eb
leadsoft <clients@leadsoft.ro>
2021-10-22 15:51:14 +0300

    Added Search Saved component

--- verdocs-web-sdk/src/components/elements/search-saved/search-saved.stories.tsx
+++ verdocs-web-sdk/src/components/elements/search-saved/search-saved.stories.tsx
@@ -2,13 +2,19 @@ import {html} from 'lit-html';
 import {Meta} from '@storybook/web-components';
 import {action} from '@storybook/addon-actions';
 
+const dummyData = [
+  { params: {q: 'nda'}, }, { params: {q: 'verdocs template', type: 'template'}, },
+]
+
 /**
  * Display a list of saved searches.
  */
 export default {
   title: 'Elements/Search/Saved',
   component: 'search-saved',
-  args: {},
+  args: {
+    options: dummyData
+  },
   argTypes: {},
 } as Meta;
 
