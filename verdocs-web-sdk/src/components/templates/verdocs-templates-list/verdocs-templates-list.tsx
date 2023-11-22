931412610033daffdf46c4e12dd1952936dad0f2
Chad Robinson <crobinson@medialantern.com>
2023-11-22 11:34:25 -0500

    Remove duplicate link

--- verdocs-web-sdk/src/components/templates/verdocs-templates-list/verdocs-templates-list.tsx
+++ verdocs-web-sdk/src/components/templates/verdocs-templates-list/verdocs-templates-list.tsx
@@ -3,11 +3,12 @@ import {VerdocsEndpoint} from '@verdocs/js-sdk';
 import {Templates} from '@verdocs/js-sdk/Templates';
 import {integerSequence} from '@verdocs/js-sdk/Utils/Primitives';
 import {ITemplate, TemplateActions} from '@verdocs/js-sdk/Templates/Types';
-import { IGetTemplateSummarySortBy, ITemplateListParams } from "@verdocs/js-sdk/Templates/Templates";
+import {IGetTemplateSummarySortBy, ITemplateListParams} from '@verdocs/js-sdk/Templates/Templates';
 import {Component, Event, EventEmitter, h, Host, Prop, State, Watch} from '@stencil/core';
 import {IFilterOption} from '../../controls/verdocs-quick-filter/verdocs-quick-filter';
 import {IMenuOption} from '../../controls/verdocs-dropdown/verdocs-dropdown';
 import {SDKError} from '../../../utils/errors';
+import {canPerformTemplateAction} from '@verdocs/js-sdk/Templates/Actions';
 
 const GlobeAltIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" /></svg>`;
 const LockClosedIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" /></svg>`;
@@ -447,21 +448,21 @@ export class VerdocsTemplatesList {
           // serverless/src/functions/getTemplates.ts:      canPerformTemplateAction(request.session, 'change_visibility_org', record) && record.allowed_operations.push('change_visibility_org');
           // serverless/src/functions/getTemplates.ts:      canPerformTemplateAction(request.session, 'change_visibility_public', record) && record.allowed_operations.push('change_visibility_public');
 
-          if (allowed_operations.includes(TemplateActions.READ) && this.allowedActions.includes('send')) {
-            MENU_OPTIONS.push({label: 'Preview / Send', id: 'send', disabled: !allowed_operations.includes(TemplateActions.READ)});
+          if (this.allowedActions.includes('send')) {
+            MENU_OPTIONS.push({label: 'Preview / Send', id: 'send', disabled: !canPerformTemplateAction(this.endpoint.session, 'read', template)});
           }
 
           // if (this.allowedActions.includes('createlink')) {
           //   MENU_OPTIONS.push({label: 'Create Link', id: 'createlink', disabled: true});
           // }
 
-          if (allowed_operations.includes(TemplateActions.CREATE_PERSONAL) && this.allowedActions.includes('signnow')) {
+          if (this.allowedActions.includes('signnow')) {
             MENU_OPTIONS.push({label: 'Sign Now', id: 'signnow', disabled: true});
           }
 
-          if (allowed_operations.includes(TemplateActions.READ) && this.allowedActions.includes('submitted')) {
+          if (this.allowedActions.includes('submitted')) {
             MENU_OPTIONS.push({label: ''});
-            MENU_OPTIONS.push({label: 'Submissions', id: 'submitted'});
+            MENU_OPTIONS.push({label: 'Submissions', id: 'submitted', disabled: !canPerformTemplateAction(this.endpoint.session, 'read', template)});
           }
 
           if (this.allowedActions.includes('link') || this.allowedActions.includes('edit') || this.allowedActions.includes('delete')) {
