045120ace97a75e6b14c917f73a5d3343378ae84
Shreyak <shreyak.chakraborty@qsstechnosoft.com>
2025-08-25 17:33:53 +0530

    added vanilla JS target and fixed rollup

---
+++ verdocs-web-sdk/bundle-entry.js
@@ -0,0 +1,4 @@
+import { defineCustomElements } from './dist/loader/index.js';
+
+// Register all components globally
+defineCustomElements(window);
--- verdocs-web-sdk/stencil.config.ts
+++ verdocs-web-sdk/stencil.config.ts
@@ -59,14 +59,11 @@ export const config: Config = {
     },
     {
       type: 'dist-custom-elements',
-      customElementsExportBehavior: 'bundle',
-      // customElementsExportBehavior: 'auto-define-custom-elements',
+      // customElementsExportBehavior: 'bundle',
+      customElementsExportBehavior: 'auto-define-custom-elements',
       generateTypeDeclarations: true,
       externalRuntime: false,
     },
-    // {
-    //   type: 'dist-custom-elements-bundle',
-    // },
     // We actually don't need these docs, we want Storybook to use "inline" docs instead. But commenting out this block
     // didn't appear to actually disable their generation. Overriding the target directory gets them out of the way so
     // storybook can do its job.
